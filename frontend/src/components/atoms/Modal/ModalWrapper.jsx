ede0f2afd91736c3e8217eb4571a339018726801
Fedorieieva Dariia <fedorieieva.dariia@lll.kpi.ua>
2024-12-18 15:28:58 +0200

    esc modal

--- frontend/src/components/atoms/Modal/ModalWrapper.jsx
+++ frontend/src/components/atoms/Modal/ModalWrapper.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PropTypes from "prop-types";
-
-import style from './style.module.scss'
+import style from './style.module.scss';
 
 const ModalWrapper = ({children, onClick}) => {
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key === "Escape") {
+                onClick();
+            }
+        };
+
+        document.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [onClick]);
+
     return (
         <div className={style.modalWrapper} onClick={onClick}>
             {children}
@@ -11,10 +24,9 @@ const ModalWrapper = ({children, onClick}) => {
     );
 };
 
-
 ModalWrapper.propTypes = {
     children: PropTypes.any,
-    onClick: PropTypes.func
-}
+    onClick: PropTypes.func.isRequired
+};
 
-export default ModalWrapper
+export default ModalWrapper;
